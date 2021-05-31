b2971f544a18bc87dff6ce7853a5096018668534
mrmilul <miladmizani@yahoo.com>
2021-05-31 20:41:36 +0300

    google client id is implemented

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -9,13 +9,32 @@ export const HomePage = () => {
         justifyContent: "center", 
         alignItems:"center"
 
+    } 
+
+    const login = (response)=>{
+        console.log(response)
     }
     return (
         <div className="home__page" style={style}>
             <div className="'login__message">
                 <h1>A Readers Favourit Place!</h1>
                 <p> We Provide high quiality online reaources</p>
-                <GoogleLogin></GoogleLogin>
+                <GoogleLogin
+            clientId = "52294641160-r9lq2d7qcvnjhrrr8mumnn4porsg969o.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="login__button"
+              >
+                Login with Google
+              </button>
+            )}
+            onSuccess={login}
+            onFailure={login}
+            isSignedIn={true}
+            cookiePolicy={"single_host_origin"}
+          />
             </div>
         </div>
     )
