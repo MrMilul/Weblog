6a7524cb591bb138f1eb9b28265f8e6ac05bab6e
mrmilul <miladmizani@yahoo.com>
2021-06-02 18:51:43 +0300

    Navbar is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { HomePage } from './components/HomePage'
+import { Navbar } from './components/Navbar'
 import './styling/app.css'
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <HomePage/>
     </div>
   )
