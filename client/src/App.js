fff2ad0c20e8bf4959d00d204c3b115455228616
mrmilul <miladmizani@yahoo.com>
2021-06-07 19:38:49 +0300

    routers are added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import {BrowserRouter as Router,
+  Switch,
+  Route} from 'react-router-dom'
+import { CreatePost } from './components/CreatePost'
 import { HomePage } from './components/HomePage'
+import { Layout } from './components/Layout'
 import { Navbar } from './components/Navbar'
 import './styling/app.css'
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <HomePage/>
-    </div>
+    <Router>
+      <Switch>
+      <Layout>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/create" component={CreatePost}/>
+      </Layout>          
+      </Switch>
+    </Router>
+    
   )
 }
 
