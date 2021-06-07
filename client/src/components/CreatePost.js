3145c1c56d0876d50820fd68fd89d08d5cca0597
mrmilul <miladmizani@yahoo.com>
2021-06-07 19:39:13 +0300

    imporoved

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,22 +1,45 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import QuilEditor from './QuilEditor'
+
 export const CreatePost = () => {
 
-    const onEditChange = ()=>{
-        console.log("onEditChange")
+    const [content, setContent] = useState("")
+    const [files, setFiles] = useState([])
+
+
+    const onEditChange = (value)=>{
+        setContent(value)
+        console.log(value)
     }
-    const onFilesChange = ()=>{
-        console.log("onFilesChange")
+    const onFilesChange = (value)=>{
+       setFiles(value)
+
+    }
+    const onSubmit = (e)=>{
+        e.preventDefault()
+
+        axios.post("/api/createPost", content).
+        then(response=>{
+            if(response){
+                console.log("content is posted")
+            }
+        }).catch(err=>{
+            console.log("content error")
+        })
     }
     return (
-        <div>
-            <h1>Editor</h1>
+        <div className="quil__editor">
+            <h1>Editor</h1><br/>
             <QuilEditor
                 placeholder={"Start Posting"}
-                onEditChange={onEditChange}
+                onEditorChange={onEditChange}
                 onFilesChange={onFilesChange}
             />
+            <form onSubmit={onSubmit}>
+                <button>Submit</button>
+            </form>
         </div>
-       
+        
     )
 }
