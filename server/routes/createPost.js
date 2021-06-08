a038734cfe3f6c44fe07e3f23d789df8d999e644
mrmilul <miladmizani@yahoo.com>
2021-06-08 18:33:20 +0300

    creating blog routes

--- server/routes/createPost.js
+++ server/routes/createPost.js
@@ -1,11 +1,38 @@
 const express = require("express")
 const Router = express.Router()
 
+
+const Blog = require('../model/posts')
 const multer = require("multer")
 
 
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`);
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.jpg' && ext !== '.png' && ext !== '.mp4') {
+            return cb(res.status(400).end('only jpg, png, mp4 is allowed'), false);
+        }
+        cb(null, true)
+    }
+});
 
-const Blog = require('../model/posts')
+const upload = multer({ storage: storage }).single("file");
+
+
+Router.post("/uploadfiles", (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.json({ success: false, err });
+        }
+        return res.json({ success: true, url: res.req.file.path, fileName: res.req.file.filename });
+    });
+});
 
 
 Router.post("/createPost", (req,res)=>{
@@ -17,7 +44,7 @@ Router.post("/createPost", (req,res)=>{
     newPost.save((err, postInfo)=>{
         if(err){
             return res.status(400).json({
-                message: err
+                message: err, 
             })
         }
         return res.status(200).json({
@@ -25,11 +52,6 @@ Router.post("/createPost", (req,res)=>{
         })
     })
 })
-
-Router.post('/uploadFiles', (req, res)=>{
-
-})
-
 
 
 
