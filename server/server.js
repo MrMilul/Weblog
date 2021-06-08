4e8eef5f135af01e62e555acc0946a9772d5b04a
mrmilul <miladmizani@yahoo.com>
2021-06-08 18:33:34 +0300

    improved

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const cors = require("cors")
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -7,9 +9,14 @@ const blogRouter = require('./routes/createPost')
 
 
 db()
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 
+app.use(cors())
 app.use('/api/blog', blogRouter)
+
+
 
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT}`)
